9fb1ebd8b5c20c7fbcbcb546fbdbf5285441b942
Nandini <nandinigupta63696@gmail.com>
2024-07-09 06:43:10 +0530

    first commit

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Home_page from './Home_page'
+
+function Landing() {
+  return (
+    <>
+    <Navbar/> 
+    <Home_page/> 
+    <Footer/>
+    </>
+  )
+}
+
+export default Landing
